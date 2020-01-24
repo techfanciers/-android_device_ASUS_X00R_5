3f18ccc5c862d1c2f514cab7504b63a7f62eb546
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    X00P: Use Pixel charger animation
    Signed-off-by: Arseniy Graur <argraur@gmail.com>
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- device.mk
+++ device.mk
@@ -289,6 +289,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti \
