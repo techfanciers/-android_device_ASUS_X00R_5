78cda7c1dcd023b28f7d43e146c42836e184122a
techfanciers <techfanciersofficial@gmail.com>
2020-07-17 14:16:53 +0000

    Updated tree for AOSP Extended

--- aosp_X00R.mk
+++ aosp_X00R.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Inherit some common Pixel Experience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 720
 
